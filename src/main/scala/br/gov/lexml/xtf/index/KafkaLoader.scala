7a8feb5ef0dcaa8f005a75def77cea78647904f1
João R. M. Nicola <joaoraf@gmail.com>
2025-09-10 08:03:07 -0300

    wip

--- src/main/scala/br/gov/lexml/xtf/index/KafkaLoader.scala
+++ src/main/scala/br/gov/lexml/xtf/index/KafkaLoader.scala
@@ -1,16 +1,19 @@
 package br.gov.lexml.xtf.index
 
+import com.github.luben.zstd.{ZstdDictCompress, ZstdOutputStream}
 import net.sf.saxon.FeatureKeys
 import net.sf.saxon.om.NamePool
 import org.cdlib.xtf.servletBase.{DTDSuppressingXMLReader, StylesheetCache}
 import org.cdlib.xtf.textEngine.IndexUtil
 import org.cdlib.xtf.textIndexer.LuceneDocBuilderHandler
 import org.cdlib.xtf.util.{Path, XTFSaxonErrorListener}
+import org.kamranzafar.jtar.{TarEntry, TarHeader, TarOutputStream}
 import org.xml.sax.InputSource
 import zio.*
 import zio.stream.*
 
-import java.io.{ByteArrayInputStream, ByteArrayOutputStream, File, IOException}
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream, File, FileOutputStream, IOException, OutputStream}
+import java.nio.charset.StandardCharsets
 import java.nio.file.{Files, Path, Paths}
 import javax.xml.transform.{OutputKeys, Templates}
 import javax.xml.transform.sax.SAXSource
@@ -35,11 +38,11 @@ object KafkaLoader extends ZIOAppDefault:
       else ZStream.empty
     makeStream(root)
 
-  def readFilePipeline : ZPipeline[Any,IOException,File,Array[Byte]] =
+  def readFilePipeline : ZPipeline[Any,IOException,File,(File,Array[Byte])] =
     ZPipeline.mapZIO { file =>
       ZIO.attemptBlockingIO {
         if file.isDirectory then throw new IOException(s"file $file is a directory!") else
-          Files.readAllBytes(file.toPath)
+          (file,Files.readAllBytes(file.toPath))
       }
     }
 
@@ -67,8 +70,8 @@ object KafkaLoader extends ZIOAppDefault:
 
   private val expectedIncrease : Double = 0.2
 
-  def transformPipeline(templates : Templates,paralelism : Int = 16) : ZPipeline[Any,Exception,Array[Byte],(Int,Either[Exception,Array[Byte]])] =
-    ZPipeline.mapZIOParUnordered[Any,Exception,Array[Byte],(Int,Either[Exception,Array[Byte]])](paralelism) { inBytes =>
+  def transformPipeline[A](templates : Templates,paralelism : Int = 16) : ZPipeline[Any,Exception,(A,Array[Byte]),(Int,A,Either[Exception,Array[Byte]])] =
+    ZPipeline.mapZIOParUnordered[Any,Exception,(A,Array[Byte]),(Int,A,Either[Exception,Array[Byte]])](paralelism) { (f,inBytes) =>
       (for {
         tf <- ZIO.attempt { templates.newTransformer() }
         _ <- ZIO.attempt {
@@ -81,7 +84,7 @@ object KafkaLoader extends ZIOAppDefault:
           tf.transform(new StreamSource(new ByteArrayInputStream(inBytes)),
             new StreamResult(baos))
         }
-      } yield baos.toByteArray).refineOrDie { case ex : Exception => ex }.either.map(x => (inBytes.length,x))
+      } yield baos.toByteArray).refineOrDie { case ex : Exception => ex }.either.map(x => (inBytes.length,f,x))
     }
 
   enum FieldValue:
@@ -116,9 +119,9 @@ object KafkaLoader extends ZIOAppDefault:
       }.toMap
       FieldMap(newFm)
 
-  def parsePipeline : ZPipeline[Any,Exception,Array[Byte],Elem] =
-    ZPipeline.mapZIO { inBytes =>
-      ZIO.attempt { XML.load(new ByteArrayInputStream(inBytes)) }.refineOrDie { case ex : Exception => ex }
+  def parsePipeline[A] : ZPipeline[Any,Exception,(A,Array[Byte]),(A,Elem)] =
+    ZPipeline.mapZIO { (f,inBytes) =>
+      ZIO.attempt { (f,XML.load(new ByteArrayInputStream(inBytes))) }.refineOrDie { case ex : Exception => ex }
     }
 
   def xmlToFieldMap(doc : Elem) : FieldMap =
@@ -139,7 +142,7 @@ object KafkaLoader extends ZIOAppDefault:
     ZPipeline.map(xmlToFieldMap)
 
 
-  override def run : ZIO[Any,Exception,Unit] =
+  /*override def run : ZIO[Any,Exception,Unit] =
     for {
       timeAndResult <- pathStream(new File("/zfs_lexml/www-data/doc"),_.getName.endsWith(".dc.xml"))
         .via(readFilePipeline)
@@ -158,7 +161,53 @@ object KafkaLoader extends ZIOAppDefault:
       _ <- ZIO.foreachDiscard(fieldMap.map(_.m).getOrElse(Map())) { (fieldName,fieldSpec) =>
         Console.printLine(s"\t$fieldName: $fieldSpec")
       }
+    } yield ()*/
+
+  override def run: ZIO[Scope, Exception, Unit] =
+    for {
+      counter <- Ref.Synchronized.make[Long](0L)
+      tos <- ZIO.acquireRelease[Any,Any,IOException,TarOutputStream](
+        ZIO.attemptBlockingIO {
+          val fos = new FileOutputStream(new File("/zfs_lexml/result/indexable-docs.tar.zstd"))
+          val zstdOs = new ZstdOutputStream(fos,9)
+          new TarOutputStream(zstdOs)
+        }
+      )( os => ZIO.attemptBlockingIO(os.close()).ignore)
+      fib <- (for {
+        timeAndResult <- pathStream(new File("/zfs_lexml/www-data/doc"), _.getName.endsWith(".dc.xml"))
+          .via(readFilePipeline)
+          .via(transformPipeline[File](dcStyleSheet))
+          .collect {
+            case (_, f, Right(v)) => (f,v)
+          }
+          .via(parsePipeline[File])
+          .runForeach { (f : File,elem : Elem) =>
+            val id = (elem \ "idDocumento").text.trim.toLong
+            val idStr = f"$id%09d"
+            val entryName = f"${idStr.substring(0,3)}/${idStr.substring(3,6)}/$idStr.xml"
+            val data = elem.toString.getBytes(StandardCharsets.UTF_8)
+            val header = TarHeader.createHeader(entryName, data.length, f.lastModified(), false, 6*64+6*8+6)
+            for {
+              _ <- ZIO.attemptBlockingIO { tos.putNextEntry(new TarEntry(header)) }
+              _ <- ZIO.attemptBlockingIO { tos.write(elem.toString.getBytes(StandardCharsets.UTF_8)) }
+              _ <- counter.update(_ + 1)
+            } yield ()
+          }
+          .timed
+        (ellapsed, _) = timeAndResult
+        _ <- Console.printLine(s"Ellapsed time: $ellapsed secs")
+        _ <- counter.set(-1)
+      } yield ()).fork
+      _ <-
+        counter.get.flatMap { cnt =>
+          Console.printLine(s"So far: $cnt") *>
+            ZIO.sleep(Duration.fromSeconds(15))
+        }.repeatUntilZIO { _ =>
+          counter.get.map(_ < 0)
+        }
+      _ <- fib.join
     } yield ()
+
 
 
 
